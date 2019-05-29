e12ce83c8deeef38a14d759b2e9f6b1a679ef034
Mannu <mannu.arneja@gmail.com>
2019-05-29 10:41:32 -0400

    add controllers

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:username][:username],
+                                     params[:username][:password])
+
+    if @user
+      login(@user)
+    #   render
+    else
+      render json: ["invalid"], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      #render
+    end
+  end
+end
