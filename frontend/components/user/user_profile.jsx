b27bc3a486b08473f33123200e2b446beb8f123e
Mannu <mannu.arneja@gmail.com>
2019-09-17 01:03:19 -0400

    prof css

--- frontend/components/user/user_profile.jsx
+++ frontend/components/user/user_profile.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 
 class UserProfile extends React.Component {
     constructor(props){
@@ -6,16 +8,26 @@ class UserProfile extends React.Component {
     }
 
     componentDidMount() {
-        console.log(this.props)
+        console.log(this.props.users[this.props.match.params.id])
     }
 
     render() {
+        const user = this.props.users[this.props.match.params.id];
         return(
             <div className="profile-container">
-                <h1>user profile test</h1>
+                <div className="profile-banner">
+                    <h3 className="profile-name">{user.username}</h3>
+                </div>
             </div>
         )
     }
 }
 
-export default UserProfile
+const msp = state => {
+    return ({
+        users: state.entities.users,
+        currentUser: state.session.id
+    });
+}
+
+export default connect(msp)(UserProfile)
