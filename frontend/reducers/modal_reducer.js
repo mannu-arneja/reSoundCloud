e53661ccc8d20e538cb51b68838b1ecd9794e8d5
Mannu <mannu.arneja@gmail.com>
2019-09-24 17:06:30 -0400

    modal fix and css

--- frontend/reducers/modal_reducer.js
+++ frontend/reducers/modal_reducer.js
@@ -1,5 +1,6 @@
 import { OPEN_MODAL, CLOSE_MODAL } from '../actions/modal_actions';
 import { RECEIVE_SESSION_ERRORS } from '../actions/session_actions';
+import { UPDATE_TIME, PAUSED } from '../actions/track_actions';
 
 const modalReducer = (state={}, action) =>{
     
@@ -9,6 +10,10 @@ const modalReducer = (state={}, action) =>{
         case CLOSE_MODAL:
             return null;
         case RECEIVE_SESSION_ERRORS:
+            return state;
+        case UPDATE_TIME:
+            return state;
+        case PAUSED:
             return state;
         default:
             return null;
