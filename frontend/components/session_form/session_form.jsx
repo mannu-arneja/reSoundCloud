123b128a9a3e1993dc10f6ab9866b64ad4911312
mannu-arneja <mannu.arneja@gmail.com>
2019-09-26 00:22:29 -0400

    dry up session modal

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -22,37 +22,12 @@ class SessionForm extends React.Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        this.props.processForm(this.state)
+        const user = Object.assign({}, this.state);
+        this.props.processForm(user)
     };
-    // handleSubmit(e) {
-    //     debugger
-    //     e.preventDefault();
-    //     this.props.processForm(this.state).then(
-    //         (closeModal()),
-    //         (() => openModal(this.props.formType))
-    //     );
-    // };
-
-    // handleContinue(e) {
-    //     e.preventDefault();
-    //     this.props.processForm(this.state).then
-    // }
-
-    // continue(){
-    //     this.props.closeModal();
-    //     this.props.openModal('login');
-    // }
 
     showErrors() {
 
-        // let errorText;
-        // if (this.props.errors.errors) {
-        //     debugger;
-        //     this.props.errors.errors.forEach(err => {
-        //         errorText = <p>{err}</p>;
-        //     });
-        // }
-        // return errorText;
         return (
             <div>
                 {this.props.errors.map((err, i) => (
@@ -63,10 +38,61 @@ class SessionForm extends React.Component {
     };
 
 
-    loginForm() {
+    // preForm() {
+    //     return(
+    //         < form onSubmit={this.handleSubmit} className="login-form" >
+    //             <label>email:
+    //             <input type="text"
+    //                     value={this.state.email}
+    //                     onChange={this.handleUpdate('email')}
+    //                     className="login-field"
+    //                 />
+    //             </label>
+    //             <br/>
+    //             <input type="submit" value="Continue" className="submit-button" />
+    //         </form >
+    //     )
+    // }
 
-        return (
-            <>
+
+    render() {
+        const signupForm = 
+        <>
+            < form onSubmit = { this.handleSubmit } className = "signup-form" >
+            <label>
+            <input type="text"
+                value={this.state.username}
+                onChange={this.handleUpdate('username')}
+                className="login-field"
+                placeholder="Your display name *"
+            />
+            </label >
+            <br/>
+            <label>
+            <input type="text"
+                value={this.state.email}
+                onChange={this.handleUpdate('email')}
+                className="login-field"
+                placeholder="Your email address *"
+            />
+            </label >
+            <br/>
+            <label>
+                <input type="password"
+                value={this.state.password}
+                onChange={this.handleUpdate('password')}
+                className="login-field"
+                placeholder="Your password *"
+            />
+            </label>
+            <br/>
+            {this.showErrors()}
+            <input type="submit" value="Continue" className="signup-button" />
+            </form >
+        </>
+
+        const loginForm =
+        <>
             < form onSubmit = { this.handleSubmit } className = "login-form" >
                 <label>
                 <input type="text"
@@ -89,75 +115,57 @@ class SessionForm extends React.Component {
                 {this.showErrors()}
                 <input type="submit" value="Continue" className="signup-button" />
             </form >
-            </>
-        )
-    }
+        </>
 
-    signupForm() {
+        // switch (this.props.formType) {
+        //     case 'login':
+        //         return loginForm;
+        //     case 'signup':
+        //         return signupForm;
+        //     default:
+        //         return null;
+        // }
+        let inputUsername = null;
+        if (this.props.formType === 'signup') {
+            inputUsername = 
+                <input type="text"
+                value={this.state.username}
+                onChange={this.handleUpdate('username')}
+                className="login-field"
+                placeholder="Your display name *"
+                />
+        }
 
         return (
+        <>
             < form onSubmit = { this.handleSubmit } className = "signup-form" >
-                <label>
-                <input type="text"
-                    value={this.state.username}
-                    onChange={this.handleUpdate('username')}
-                    className="login-field"
-                    placeholder="Your display name *"
-                />
-                </label >
-                <br/>
-                <label>
-                <input type="text"
-                    value={this.state.email}
-                    onChange={this.handleUpdate('email')}
-                    className="login-field"
-                    placeholder="Your email address *"
-                />
-                </label >
-                <br/>
-                <label>
-                    <input type="password"
-                    value={this.state.password}
-                    onChange={this.handleUpdate('password')}
-                    className="login-field"
-                    placeholder="Your password *"
-                />
-                </label>
-                <br/>
-                {this.showErrors()}
-                <input type="submit" value="Continue" className="signup-button" />
+            <label>
+                {inputUsername}
+            </label >
+            <br/>
+            <label>
+            <input type="text"
+                value={this.state.email}
+                onChange={this.handleUpdate('email')}
+                className="login-field"
+                placeholder="Your email address *"
+            />
+            </label >
+            <br/>
+            <label>
+                <input type="password"
+                value={this.state.password}
+                onChange={this.handleUpdate('password')}
+                className="login-field"
+                placeholder="Your password *"
+            />
+            </label>
+            <br/>
+            {this.showErrors()}
+            <input type="submit" value="Continue" className="signup-button" />
             </form >
+        </>
         )
-    }
-
-    // preForm() {
-    //     return(
-    //         < form onSubmit={this.handleSubmit} className="login-form" >
-    //             <label>email:
-    //             <input type="text"
-    //                     value={this.state.email}
-    //                     onChange={this.handleUpdate('email')}
-    //                     className="login-field"
-    //                 />
-    //             </label>
-    //             <br/>
-    //             <input type="submit" value="Continue" className="submit-button" />
-    //         </form >
-    //     )
-    // }
-
-
-    render() {
-        switch (this.props.formType) {
-            case 'email':
-                return (this.preForm());
-            case 'login':
-                return (this.loginForm());
-            case 'signup':
-                return (this.signupForm());
-            default:
-                return null;
-        }
     }
 
 };
